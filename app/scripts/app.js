57e9c38d6258bdcdf67ac0cb117a58d087c3b2f1
bichotll <jaume@bic.cat>
2015-02-14 16:48:53 +0100

    - added google-chart dependency - added test google-chart in balance page - added objectives n statistics options to menu

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('TestOsperIonic', ['ionic', 'config'])
+angular.module('TestOsperIonic', ['ionic', 'googlechart', 'config'])
 
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
--- app/scripts/controllers/card-balance-controller.js
+++ app/scripts/controllers/card-balance-controller.js
@@ -2,7 +2,7 @@
 angular.module('TestOsperIonic')
 
   .controller('CardBalanceCtrl', function ($scope, $ionicLoading, $timeout, $ionicHistory, osperApiFactory) {
-    var init = function(){
+    var init = function () {
       $ionicLoading.show();
 
       $ionicHistory.nextViewOptions({
@@ -21,6 +21,116 @@ angular.module('TestOsperIonic')
         $scope.lastMovements = osperApiFactory.lastMovements();
         $ionicLoading.hide();
       }, 1000);
+
+      $scope.chartObject = {
+        "type": "AreaChart",
+        "displayed": true,
+        "data": {
+          "cols": [
+            {
+              "id": "month",
+              "label": "Month",
+              "type": "string",
+              "p": {}
+            },
+            {
+              "id": "balance-id",
+              "label": "Balance",
+              "type": "number",
+              "p": {}
+            },
+            {
+              "id": "bike-objective-id",
+              "label": "Bike objective",
+              "type": "number",
+              "p": {}
+            },
+            {
+              "id": "phone-objective-id",
+              "label": "Phone objective",
+              "type": "number",
+              "p": {}
+            }
+          ],
+          "rows": [
+            {
+              "c": [
+                {
+                  "v": "1st week"
+                },
+                {
+                  "v": 40.15
+                },
+                {
+                  "v": 10
+                },
+                {
+                  "v": 5
+                }
+              ]
+            },
+            {
+              "c": [
+                {
+                  "v": "2nd week"
+                },
+                {
+                  "v": 10.20
+                },
+                {
+                  "v": 15
+                },
+                {
+                  "v": 7
+                }
+              ]
+            },
+            {
+              "c": [
+                {
+                  "v": "3rd week"
+                },
+                {
+                  "v": 5.60
+                },
+                {
+                  "v": 20
+                },
+                {
+                  "v": 14
+                }
+              ]
+            },
+            {
+              "c": [
+                {
+                  "v": "4th week"
+                },
+                {
+                  "v": 8.50
+                },
+                {
+                  "v": 25
+                },
+                {
+                  "v": 16
+                }
+              ]
+            }
+          ]
+        },
+        "options": {
+          "title": "last 4 weeks balance",
+          "fill": 20,
+          "displayExactValues": true,
+          "vAxis": {
+          },
+          "hAxis": {
+            "title": "Date"
+          }
+        },
+        "formatters": {}
+      }
     }
     init();
   });
